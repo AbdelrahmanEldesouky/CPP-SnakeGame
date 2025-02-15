eb48fb5b9b40cef5b48b4555e53bb02d31620286
AbelrahmanEldesouky <abdelrhman.aldesoky@outlook.com>
2025-02-15 22:54:18 +0200

    feat: change member variables in renderer class to non-const for flexibility

--- src/renderer.h
+++ src/renderer.h
@@ -32,10 +32,10 @@ private:
   SDL_Window *sdl_window;
   SDL_Renderer *sdl_renderer;
 
-  const std::size_t screen_width;
-  const std::size_t screen_height;
-  const std::size_t grid_width;
-  const std::size_t grid_height;
+  std::size_t screen_width;
+  std::size_t screen_height;
+  std::size_t grid_width;
+  std::size_t grid_height;
 };
 
 #endif
